b0fcffaf6f4bc7fd7bb8802bf45a385af8578be3
Chinmay Manchanda <46476694+ChinmayManchanda@users.noreply.github.com>
2020-03-28 23:49:33 +1100

    Xifie Changes done

--- SourceCode_and_Documentation/src/ticket_master.py
+++ SourceCode_and_Documentation/src/ticket_master.py
@@ -3,7 +3,7 @@ from event import Event
 from datetime import datetime, timedelta
 
 URL = 'https://app.ticketmaster.com/discovery/v2/events'
-
+API_KEY = 's1zvxw6AwOPPr7ej3vaA4EykE2LeO0vU'
 CLASSIFICATIONS = [
     'Hip-Hop',
     'Arts & Theatre'
@@ -16,22 +16,21 @@ def get_events():
         # Parameters for get request which includes , apikey, classifications and event date times.
         client_info = dict(
             classificationName=classification,
-            apikey = 's1zvxw6AwOPPr7ej3vaA4EykE2LeO0vU',
+            apikey = API_KEY,
             source='ticketmaster',
             countryCode='AU',
             stateCode='NSW',
-            StartDateTime=datetime.now().isoformat()[:-7] + 'Z',
-            EndDateTime=(datetime.now() + timedelta(days=1)
-                         ).isoformat()[:-7] + 'Z'
+            startDateTime=datetime.now().isoformat()[:-7] + 'Z',
+            endDateTime=(datetime.now() + timedelta(days=1)).isoformat()[:-7] + 'Z'
         )
 
         response = requests.get(url=URL, params=client_info)
         data = json.loads(response.text)
         
         if '_embedded' in data:
-            event = data['_embedded']['events']
+            events = data['_embedded']['events']
             
-            for info in event:
+            for info in events:
                 event_obj = parseInfoToEvent(info)
                 event_list.append(event_obj)
 
@@ -47,7 +46,7 @@ def parseInfoToEvent(info):
     latitude = info['_embedded']['venues'][0]['location']['latitude']
     longitude = info['_embedded']['venues'][0]['location']['longitude']
     is_online = True
-    price = False
+    is_free = False  #this parameter is always false in ticketmaster API because ticketmaster only returns events which have tickets.
     
     if info['dates']['status']['code'] == 'cancelled':
         is_online = False
@@ -66,6 +65,6 @@ def parseInfoToEvent(info):
         tags = [call['segment']['name'],call['genre']['name'],call['subGenre']['name']]
     
     event_info = Event(info['id'],url, start_time, end_time, latitude, longitude, 
-        info['name'], organiser ,price, is_online, summary, tags, [])
+        info['name'], organiser ,is_free, is_online, summary, tags, [])
 
     return event_info
