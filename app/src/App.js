d6873f729d779ab70afc28a822c810f99152cf8d
Marina Reshetnikova <61497679+colageyo@users.noreply.github.com>
2020-04-16 14:04:18 +1000

    Update app/src/App.js
    Co-Authored-By: Cecilia the Cat <cecilia990@outlook.com>

--- app/src/App.js
+++ app/src/App.js
@@ -18,7 +18,6 @@ class App extends Component {
       <div className="App">
       <HelloWorldToolBar />
         <Route exact path='/' component={HomePage} />
-      
       </div>
       </Router>
 
