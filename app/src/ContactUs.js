557ce9992ee8b4bd40cc04c2182f12d813198f37
XifeiNi <cecilia990@outlook.com>
2020-04-16 12:17:17 +1000

    Work on desktop

--- app/src/ContactUs.js
+++ app/src/ContactUs.js
@@ -3,46 +3,20 @@ import './ContactUs.css';
 import './App.css';
 
 class ContactUsPage extends Component {
-    constructor() {
-        super();
-        this.state = {
-          width: window.innerWidth,
-        };
-    }
-      
-    componentWillMount() {
-        window.addEventListener('resize', this.handleWindowSizeChange);
-    }
-    
-    componentWillUnmount() {
-        window.removeEventListener('resize', this.handleWindowSizeChange);
-    }
-    
-    handleWindowSizeChange = () => {
-        this.setState({ width: window.innerWidth });
-    };
-
     render() {
-        const { width } = this.state;
-        const isMobile = width <= 500;
-        if (isMobile) {
-            return (
-                <div>
-
-                </div>
-            );
-        } else {
             return (
                 <div className="contact-us-outer-container">
                     <div className="contact-us-inner-container">
-                       <span id="title" className="text"> Get in Touch</span> 
-
+                            <span id="title" className="text"> Get in Touch</span> 
+                            <input className="input text" type="text" id="name" name="name" placeholder="Name"/>
+                            <input className="input text" type="text" id="email" name="email" placeholder="Email"/>
+                            <span className="text message-text">Message</span>
+                            <textarea className="message" name="msg"></textarea>
+                            <div className="send-request text button">Send Request</div>
                     </div>
                 </div>
             );
-        }
     }
 }
-
 
 export default ContactUsPage;
