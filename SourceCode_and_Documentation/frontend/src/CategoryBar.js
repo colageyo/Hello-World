3d737ef6dc77763e59236a4f3ab34ddfc36f1913
XifeiNi <cecilia990@outlook.com>
2020-04-19 19:34:02 +1000

    Format all documents

--- SourceCode_and_Documentation/frontend/src/CategoryBar.js
+++ SourceCode_and_Documentation/frontend/src/CategoryBar.js
@@ -1,38 +1,44 @@
-import React, { Component }  from 'react';
-import CategoryButton from './CategoryButton';
-import './App.css';
-import './CategoryBar.css';
+import React, { Component } from "react";
+import CategoryButton from "./CategoryButton";
+import "./App.css";
+import "./CategoryBar.css";
 
-const categories = ["family-friendly", "artsy", "delicious", "geeky", "historic", "indoors", "outdoors", "romantic", "sporty"];
+const categories = [
+  "family-friendly",
+  "artsy",
+  "delicious",
+  "geeky",
+  "historic",
+  "indoors",
+  "outdoors",
+  "romantic",
+  "sporty"
+];
 
 class CategoryBar extends Component {
-    constructor(props) {
-        super(props);
-        this.state = new Map(categories.map(cat => [cat, false]));
-        this.handleClick = this.handleClick.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = new Map(categories.map(cat => [cat, false]));
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-    handleClick= (category) => {
-        this.setState((state) => ({
-            [category]: !state[category]
-         }));
-    }
+  handleClick = category => {
+    this.setState(state => ({
+      [category]: !state[category]
+    }));
+  };
 
-    render() {
-        const categoryButtons = categories.map(category => (
-            <CategoryButton
-                key={category}
-                category={category}
-                onClick={this.handleClick}
-                value={this.state[category]}
-            />
-        ));
-        return (
-            <div className="category-bar-scroll-filter">
-                {categoryButtons} 
-            </div>
-        );
-    }
+  render() {
+    const categoryButtons = categories.map(category => (
+      <CategoryButton
+        key={category}
+        category={category}
+        onClick={this.handleClick}
+        value={this.state[category]}
+      />
+    ));
+    return <div className="category-bar-scroll-filter">{categoryButtons}</div>;
+  }
 }
 
 export default CategoryBar;
--- SourceCode_and_Documentation/frontend/src/CategoryButton.js
+++ SourceCode_and_Documentation/frontend/src/CategoryButton.js
@@ -1,5 +1,5 @@
-import React, {Component} from "react";
-import { CovidDialog } from './CovidDialog';
+import React, { Component } from "react";
+import { CovidDialog } from "./CovidDialog";
 
 import "./CategoryButton.css";
 
@@ -26,50 +26,59 @@ const icons = {
 };
 
 class CategoryButton extends Component {
-    constructor(props) {
-        super(props);
-        this.handleClick = this.handleClick.bind(this);
-        this.state = {
-            x: 0,
-            y: 0,
-            isDisabled: this.props.category == "outdoors"
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      x: 0,
+      y: 0,
+      isDisabled: this.props.category == "outdoors"
+    };
+  }
+
+  handleClick = () => {
+    this.props.onClick(this.props.category);
+  };
+
+  onMouseMove = e => {
+    this.setState({
+      x: e.clientX,
+      y: e.clientY
+    });
+  };
+
+  onMouseLeave = () => {
+    this.setState(() => ({
+      x: 0,
+      y: 0
+    }));
+  };
+
+  render() {
+    const covidLog =
+      this.state.isDisabled && this.state.x != 0 && this.state.y != 0 ? (
+        <CovidDialog top={this.state.y} left={this.state.x} />
+      ) : null;
+    return (
+      <div
+        className={
+          this.props.value ? "category-button click-effect" : "category-button"
         }
-    }
-
-    handleClick= () => {
-      this.props.onClick(this.props.category); 
-    }
-
-    onMouseMove = (e) => {
-        this.setState({
-            x: e.clientX,
-            y: e.clientY
-        });
-    }  
-    
-    onMouseLeave = () => {
-        this.setState(() => ({
-            x: 0,
-            y: 0
-        }));
-    }
-    
-
-    render() {
-        const covidLog = this.state.isDisabled && this.state.x != 0 && this.state.y != 0 ? <CovidDialog top={this.state.y} left={this.state.x} /> : null;
-        return (
-            <div className={this.props.value ? 'category-button click-effect': 'category-button'} 
-                onClick={this.handleClick} 
-                onMouseMove={this.onMouseMove} 
-                onMouseLeave={this.onMouseLeave}
-            >
-                {covidLog}
-              <div className="category-text text">
-                {this.props.category} <img className="category-image" src={icons[this.props.category]}></img>
-              </div>
-            </div>
-          );
-    }
+        onClick={this.handleClick}
+        onMouseMove={this.onMouseMove}
+        onMouseLeave={this.onMouseLeave}
+      >
+        {covidLog}
+        <div className="category-text text">
+          {this.props.category}{" "}
+          <img
+            className="category-image"
+            src={icons[this.props.category]}
+          ></img>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default CategoryButton;
